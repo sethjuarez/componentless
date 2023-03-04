f079589c0e54e3e9c6d95afb92c080520eb01f7f
sethjuarez <me@sethjuarez.com>
2023-03-03 17:07:53 -0800

    initial commit with base app, auth, and logging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import AzureAppInsights from "@components/appinsights";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AzureAppInsights>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </AzureAppInsights>
+  );
 }
